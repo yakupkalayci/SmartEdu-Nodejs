10c0b39c8cd06f438e7aa29072ab187cafd758e4
yakupkalayci <ykpklyc18@gmail.com>
2022-09-05 13:59:30 +0300

    import static files and template engine

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const ejs = require("ejs");
 
 const app = express();
 
+//Middlewares
+app.use(express.static("public"));
+
+//Template Engine
+app.set("view engine", "ejs"); 
+
+//Routes
 app.get("/", (req, res) => {
-    res.status(200).send("INDEX Sayfası");
+    res.status(200).render("index", {
+        page_name: "index"
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.status(200).render("about", {
+        page_name: "about"
+    });
 });
 
 
